a2fa3c1293d2838e99389eacb44656ecc00d50fa
n0pe-sled <julian@inspired-sec.com>
2023-08-22 10:30:18 -0400

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Update and install necessary packages
-apt-get update && apt-get upgrade -y && apt-get install -y \
+sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install -y \
     python3 \
     python3-venv \
     curl \
@@ -15,17 +15,18 @@ apt-get update && apt-get upgrade -y && apt-get install -y \
     build-essential \
     unzip \
     zsh 
+
 chsh -s $(which zsh)
 
 # Install GCloud CLI
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
-apt-get update -y && apt-get install google-cloud-sdk -y
+sudo apt-get update -y && sudo apt-get install google-cloud-sdk -y
 
 # Install AWS CLI
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
-./aws/install
+sudo ./aws/install
 
 # Install Azure CLI
 curl -sL https://aka.ms/InstallAzureCLIDeb | bash
@@ -36,14 +37,14 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 # Install Go
 curl -O https://dl.google.com/go/go1.21.0.linux-amd64.tar.gz
 tar xvf go1.21.0.linux-amd64.tar.gz
-chown -R root:root ./go
-mv go /usr/local
+sudo chown -R root:root ./go
+sudo mv go /usr/local
 
 export PATH=$PATH:/usr/local/go/bin/
 
 # Install Nodejs
 curl -fsSL https://deb.nodesource.com/setup_lts.x | bash -
-apt-get install -y nodejs
+sudo apt-get install -y nodejs
 
 # Setup Powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.powerlevel10k 
